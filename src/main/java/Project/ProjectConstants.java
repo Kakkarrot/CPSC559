7e4aca7fee984ecb66ea408a72dc623a87c33f40
Kakkarrot <lqf5474@gmail.com>
2022-01-23 18:06:43 -0700

    added constant information to constants file

---
+++ src/main/java/Project/ProjectConstants.java
@@ -0,0 +1,7 @@
+package Project;
+
+public class ProjectConstants {
+    public static final String TEAM_NAME = "Kakkarrot";
+    public static final String SERVER_URL = "136.159.5.22";
+    public static final int SERVER_PORT = 55921;
+}

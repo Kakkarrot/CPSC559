c0a98f7abd9f1ac4ae9220868f7ad71525392559
Kakkarrot <lqf5474@gmail.com>
2022-01-23 22:41:54 -0700

    completed iteration 1. Nothing left but to submit during the window

--- src/main/java/Project/Iteration1/ProjectClient.java
+++ src/main/java/Project/Iteration1/ProjectClient.java
@@ -5,6 +5,8 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class ProjectClient {
     public static final String DIRECTORY = "/Users/willieli/CPSC559/src/main/java/Project/Iteration1";
@@ -12,11 +14,13 @@ public class ProjectClient {
     private Socket socket;
     private BufferedReader readSocket;
     private BufferedWriter writeSocket;
+    private CopyOnWriteArraySet<Peer> peers;
 
     public void connectToServer(String serverUrl) throws IOException {
         socket = new Socket(serverUrl, ProjectConstants.SERVER_PORT);
         readSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         writeSocket = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        peers = new CopyOnWriteArraySet<>();
     }
 
     public void disconnectFromServer() throws IOException {
@@ -26,8 +30,7 @@ public class ProjectClient {
     }
 
     private String getServerResponse() throws IOException {
-        String message = readSocket.readLine();
-        return message;
+        return readSocket.readLine();
     }
 
     private void sendServerMessage(String message) throws IOException {
@@ -37,17 +40,21 @@ public class ProjectClient {
 
     public void handleProjectIteration1Logic() throws IOException {
         for (String message = getServerResponse(); message != null; message = getServerResponse()) {
-            System.out.println(message);
             switch (message) {
                 case "get team name" -> sendServerMessage(getTeamNameRequestMessage());
                 case "get code" -> sendServerMessage(getCodeRequestMessage());
+                case "receive peers" -> processRecieveRequest();
+                case "get report" -> sendServerMessage(getReportRequestMessage());
+                case "close" -> {
+                    return;
+                }
             }
         }
     }
 
-    private String getFilesAsStringFrom(String path) throws IOException {
+    private String getFilesAsStringFrom() throws IOException {
         StringBuilder result = new StringBuilder();
-        File directory = new File(path);
+        File directory = new File(ProjectClient.DIRECTORY);
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
@@ -69,26 +76,55 @@ public class ProjectClient {
         String language = "Java\n";
         String endOfCode = "...\n";
         return language +
-                getFilesAsStringFrom(DIRECTORY) +
+                getFilesAsStringFrom() +
                 endOfCode;
     }
 
-    private void handleRecieveRequest() {
-
+    private void processRecieveRequest() throws IOException {
+        String numberOfPeers = getServerResponse();
+        for (int i = 0; i < Integer.parseInt(numberOfPeers); i++) {
+            String response = getServerResponse();
+            String[] temp = response.split(":");
+            Peer peer = new Peer(temp[0], Integer.parseInt(temp[1]));
+            peers.add(peer);
+        }
     }
 
-    private void handleReportRequest() {
-
+    private String getSingleSourceForInteration1() {
+        return "1\n"
+                + ProjectConstants.SERVER_URL
+                + ':'
+                + ProjectConstants.SERVER_PORT
+                + '\n'
+                + LocalDateTime.now() +
+                '\n';
     }
 
-    private void handleCloseRequest() {
+    private String getAllPeers() {
+        StringBuilder message = new StringBuilder();
+        message.append(peers.size());
+        message.append('\n');
+        for (Peer peer : peers) {
+            message.append(peer.getAddress());
+            message.append(':');
+            message.append(peer.getPort());
+            message.append('\n');
+        }
+        return message.toString();
+    }
 
+    private String getReportRequestMessage() {
+        String message = "";
+        message += getAllPeers();
+        message += getSingleSourceForInteration1();
+        message += getAllPeers();
+        return message;
     }
 
     public static void main(String[] args) {
         ProjectClient client = new ProjectClient();
         try {
-            client.connectToServer("localhost");
+            client.connectToServer(ProjectConstants.SERVER_URL);
             client.handleProjectIteration1Logic();
         } catch (IOException e) {
             e.printStackTrace();
